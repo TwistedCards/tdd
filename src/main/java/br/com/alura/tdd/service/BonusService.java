f50ea6962b76f6547d3dbb162b38ca923684f6b6
TwistedCards <paulo.100potter@gmail.com>
2022-03-21 11:01:27 -0300

    Test whit exception

--- src/main/java/br/com/alura/tdd/service/BonusService.java
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -9,9 +9,14 @@ public class BonusService {
 
 	public BigDecimal calculateBonus(Employee employee) {
 		BigDecimal value = employee.getWage().multiply(new BigDecimal("0.1"));
+		/*
+		 * if (value.compareTo(new BigDecimal("1000")) > 0) { value = BigDecimal.ZERO; }
+		 */
+		
 		if (value.compareTo(new BigDecimal("1000")) > 0) {
-			value = BigDecimal.ZERO;
+			throw new IllegalArgumentException("Funcionário com salario maior do que R$1000 não pode receber bonus");
 		}
+		
 		return value.setScale(2, RoundingMode.HALF_UP);// 2 casas decimais e arredonda para cima
 	}
 
--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,6 +1,8 @@
 package br.com.alura.tdd.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -12,17 +14,43 @@ import br.com.alura.tdd.modelo.Employee;
 class BonusServiceTest {
 
 	/**
-	 * Bonus deveria ser zero para funcionario com salario muito alto
+	 * Bonus deveria ser zero para funcionario com salario muito alto, mas testando com exception
 	 * 
 	 * @author paulo
 	 */
 	@Test
-	void bonusShouldBeZeroForEmployeeWithSalaryVeryHigh() {
+	void bonusShouldBeZeroForEmployeeWithSalaryVeryHighButWhitExceptionNow() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calculateBonus(new Employee("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
 		
-		assertEquals(new BigDecimal("0.00"), bonus);
+		// First way to do it
+		assertThrows(IllegalArgumentException.class, 
+						() -> service.calculateBonus(new Employee("Rodrigo", LocalDate.now(), new BigDecimal("25000")))
+					);
+		
+		// Second way to do it
+//		try {
+//			service.calculateBonus(new Employee("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+//			fail("Não deu a exception!");
+//		} catch (Exception e) {
+//			assertEquals("Funcionário com salario maior do que R$1000 não pode receber bonus", e.getMessage());
+//		}
+		
 	}
+	
+	/**
+	 * Bonus deveria ser zero para funcionario com salario muito alto
+	 * 
+	 * @author paulo
+	 */
+//	@Test
+//	void bonusShouldBeZeroForEmployeeWithSalaryVeryHigh() {
+//		BonusService service = new BonusService();
+//		BigDecimal bonus = service.calculateBonus(new Employee("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+//		
+//		assertEquals(new BigDecimal("0.00"), bonus);
+//	}
+	
+	
 	
 	/**
 	 * Bonus deveria ser 10 por centro do salario
