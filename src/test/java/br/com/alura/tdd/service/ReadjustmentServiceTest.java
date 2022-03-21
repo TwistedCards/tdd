3143adcab87b41abea00ea34f591101c18f1b44c
TwistedCards <paulo.100potter@gmail.com>
2022-03-21 11:13:47 -0300

    Organize test, annotations for initialize

--- src/test/java/br/com/alura/tdd/service/ReadjustmentServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/ReadjustmentServiceTest.java
@@ -5,12 +5,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Desempenho;
 import br.com.alura.tdd.modelo.Employee;
 
 public class ReadjustmentServiceTest {
+	
+	private ReadjustmentService service;
+	private Employee employee;
+
+	// Roda toda vez antes de iniciar um metodo
+	@BeforeEach
+	public void inicializar() {
+		System.out.println("inicializa");
+		this.service = new ReadjustmentService();
+		this.employee = new Employee("Paul", LocalDate.now(), new BigDecimal("1000.00"));
+	}
+	
+	// Roda toda vez após terminar um metodo
+	@AfterEach
+	public void finalizar() {
+		System.out.println("Fim");
+	}
+	
+	// Roda uma unica vez primeiro que todo mundo
+	@BeforeAll
+	public static void antesDeTodos() {
+		System.out.println("ANTES DE TODOS");
+	}
+	
+	// Roda uma unica vez assim que finaliza todos os testes
+	@AfterAll
+	public static void depoisDeTodos() {
+		System.out.println("DEPOIS DE TODOS");
+	}
 	
 	/**
 	 * O sistema deve permitir que os funcionários recebam um ajuste salarial anual baseado
@@ -23,27 +56,18 @@ public class ReadjustmentServiceTest {
 	
 	@Test
 	public void readjustShouldBeThreePercentWhenPerformanceForDesirable() {
-		ReadjustmentService service = new ReadjustmentService();
-		Employee employee = new Employee("Paul", LocalDate.now(), new BigDecimal("1000.00"));
-		
 		service.grantReadjustment(employee, Desempenho.DESIRABLE);
 		assertEquals(new BigDecimal("1030.00"), employee.getWage());
 	}
 	
 	@Test 
 	public void readjustShouldBeFifteenPercentWhenPerformanceForGood() {
-		ReadjustmentService service = new ReadjustmentService();
-		Employee employee = new Employee("Paul", LocalDate.now(), new BigDecimal("1000.00"));
-		
 		service.grantReadjustment(employee, Desempenho.GOOD);
 		assertEquals(new BigDecimal("1150.00"), employee.getWage());
 	}
 	
 	@Test 
 	public void readjustShouldBeTwentyPercentWhenPerformanceForGreat() {
-		ReadjustmentService service = new ReadjustmentService();
-		Employee employee = new Employee("Paul", LocalDate.now(), new BigDecimal("1000.00"));
-		
 		service.grantReadjustment(employee, Desempenho.GREAT);
 		assertEquals(new BigDecimal("1200.00"), employee.getWage());
 	}
